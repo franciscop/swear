a25fa282025c357fdc392572a410a7743eadad7b
Francisco Presencia <public@francisco.io>
2018-09-13 01:21:27 +0900

    Improved scripts and meta info

--- magic.js
+++ magic.js
@@ -1,13 +1,13 @@
-// // magic-promises 0.1 (https://github.com/franciscop/magic)
-// // @author Francisco Presencia (https://francisco.io/) <public@francisco.io>
+// magic-promises (https://github.com/franciscop/magic-promises)
+// @author Francisco Presencia (https://francisco.io/) <public@francisco.io>
 
 // Bind it to retrieve the original line
 // const log = console.log.bind(global);
-const log = () => {};
+// const log = () => {};
 
 // Resolve a bit recursively
 const resolve = async value => {
-  log('RESOLVING', value);
+  // log('RESOLVING', value);
   value = await value;
   if (Array.isArray(value)) {
     return await Promise.all(value.map(resolve));
@@ -21,18 +21,18 @@ const reject = message => Promise.reject(new Error(message));
 
 // For Proxy's { get } trap (property access)
 const getter = obj => (target, key) => {
-  log('GETTER', obj, key, typeof key);
+  // log('GETTER', obj, key, typeof key);
 
   // This expects to return a function, that can resolve to another promise
   // const res = await magic(obj); magic(obj).then(res => {...});
   if (key === 'then') return (...args) => {
-    log('GETTER THEN', obj, ...args);
+    // log('GETTER THEN', obj, ...args);
     // resolve(obj).then(res => log('AAAA', res));
     return resolve(obj).then(...args);
   };
 
   if (key === 'catch') return (...args) => {
-    log('GETTER CATCH', obj, ...args);
+    // log('GETTER CATCH', obj, ...args);
     // if (name === 'catch') return (...args) => play(prom.catch(...args), opts);
     return root(resolve(obj).catch(...args));
   };
@@ -45,13 +45,13 @@ const getter = obj => (target, key) => {
     // Returns the requested FUNCTION; so we need to bind the context
     // const res = magic(obj).map(item => {...});
     if (obj[key] && obj[key].bind) {
-      log('GETTER FUNCTION', typeof obj, obj, key);
+      // log('GETTER FUNCTION', typeof obj, obj, key);
       return func(obj[key].bind(obj));
     }
 
     // Return the simple property
     // const res = await magic(obj).key;
-    log('GETTER PROPERTY', obj, key);
+    // log('GETTER PROPERTY', obj, key);
     return func(obj[key]);
   }));
 };
@@ -60,7 +60,7 @@ const getter = obj => (target, key) => {
 
 // For Proxy()'s { apply } trap (function call)
 const applier = obj => (target, self, args) => {
-  log('APPLIER', obj, args);
+  // log('APPLIER', obj, args);
 
   // Wrap it in a resolution so promises are first-class values
   return func(resolve(obj).then(obj => {
